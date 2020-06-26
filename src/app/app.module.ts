65f821a8e0cfa02c594520d202af93f16f41ae19
apheliana <65423514+apheliana@users.noreply.github.com>
2020-06-26 21:48:44 +0300

    your-lists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/your-lists.page.ts
+++ src/app/your-lists.page.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'your-lists',
   templateUrl: './your-lists.page.html',
-  styleUrls: ['./your-lists.page.sass']
+  styleUrls: ['./your-lists.page.scss']
 })
 export class YourListsComponent {
   title = 'Your Lists';
+}
+
+export class List {
+  id: number = 0; //unique
+  name: string = '';
+  listItem: ListItem[] = [];
+  dateCreated: Date = new Date();
+  dateModified: Date = new Date();
+  dateDeleted: Date = new Date();
+}
+
+export class ListItem {
+  id: number; //unique
+  listId: number; //which list is this item in
+  titleText: string;
+  titleURL: string; //optional
+  dateActual: Date = new Date();
+  dateCreated: Date = new Date();
+  dateModified: Date = new Date(); //could we be able see earlier versions of the item?
+  dateDeleted: Date = new Date();
+
 }
