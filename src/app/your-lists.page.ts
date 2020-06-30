993c5e0cd602acf8baeb4c5370539f945d81f903
apheliana <65423514+apheliana@users.noreply.github.com>
2020-07-01 02:21:56 +0300

    main page
    storage files

--- src/app/your-lists.page.ts
+++ src/app/your-lists.page.ts
@@ -13,38 +13,70 @@ export class YourListsComponent {
   listName: string = '';
   titleText: string = '';
   titleURL: string = '';
-  lists = new FormControl();
-  listOfLists: string[] = ['Articles', 'Books', 'TV Shows'];
-  myListItem: ListItem[];
-  myList: List[];
-  
+  yourListItems: ListItem[] = [];
+  yourLists: List[] = [];
+  listIdCounter: number = 0;
+  listItemIdCounter: number = 0;
+  whichList: number = 0;
+  viewLists: string = 'no';
+  viewListItems: string = 'no';
 
   constructor() {
-    !this.myList ? console.log('do nothing') : console.log('show table');
-    !this.myListItem ? console.log('no list item') : console.log('list item constructed');
+    const yourListsJSON = localStorage.getItem('your-lists');
+    const yourLists = JSON.parse(yourListsJSON) as List[];
+
+    if (yourLists !== null) {
+      this.viewLists = 'yes';
+      this.yourLists = yourLists.map(dataItem => {
+        const yourList = new List();
+        yourList.id = dataItem.id;
+        if (dataItem.id >= this.listIdCounter) { this.listIdCounter = dataItem.id + 1; }
+        yourList.name = dataItem.name;
+        return yourList;
+      });
+    }
+  
+    const yourListItemsJSON = localStorage.getItem('your-list-items');
+    const yourListItems = JSON.parse(yourListItemsJSON) as ListItem[];
+
+    if (yourListItems !== null) {
+      this.viewListItems = 'yes';
+      this.yourListItems = yourListItems.map(dataItem => {
+        const yourListItem = new ListItem();
+        yourListItem.id = dataItem.id;
+        if (dataItem.id >= this.listItemIdCounter) { this.listItemIdCounter = dataItem.id + 1; }
+        yourListItem.listId = dataItem.listId;
+        yourListItem.titleText = dataItem.titleText;
+        return yourListItem;
+      });
+    }
   }
   
   addNewListItem(): void {
-    const myListItem = new ListItem();
-    myListItem.id = 0;
-    myListItem.listId = 123;
-    myListItem.titleText = this.titleText;
-    myListItem.titleURL = this.titleURL;
-    myListItem.dateActual = this.dateActual;
-    myListItem.dateCreated = new Date();
-    myListItem.dateModified = new Date();
-    myListItem.dateDeleted = new Date();
-    console.log('list item:', myListItem); 
+    const yourListItem = new ListItem();
+    yourListItem.id = this.listItemIdCounter;
+    this.listItemIdCounter++;
+    yourListItem.listId = this.whichList;
+    yourListItem.titleText = this.titleText;
+    yourListItem.titleURL = this.titleURL;
+    yourListItem.dateActual = this.dateActual;
+    yourListItem.dateCreated = new Date();
+    yourListItem.dateModified = new Date();
+    yourListItem.dateDeleted = new Date();
+    this.yourListItems.push(yourListItem);
+    localStorage.setItem('your-list-items', JSON.stringify(this.yourListItems));
   }
 
   addNewList(): void {
-    const myList = new List();
-    myList.id = 123;
-    myList.name = this.listName;
-    myList.dateCreated = new Date();
-    myList.dateModified = new Date();
-    myList.dateDeleted = new Date();
-    console.log('list:', myList);
+    const yourList = new List();
+    yourList.id = this.listIdCounter;
+    this.listIdCounter++;
+    yourList.name = this.listName;
+    yourList.dateCreated = new Date();
+    yourList.dateModified = new Date();
+    yourList.dateDeleted = new Date();
+    this.yourLists.push(yourList);
+    localStorage.setItem('your-lists', JSON.stringify(this.yourLists));
   }
 }
 
