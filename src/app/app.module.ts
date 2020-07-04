cf005fb1836af8a466f4a9a2925482b7104909d3
coni2k <coni2k@users.noreply.github.com>
2020-07-04 23:47:56 +0300

    MatButton fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-//angular material
+// Angular Material
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 //pages
 import { YourListsComponent } from './your-lists.page';
@@ -28,13 +29,13 @@ import { YourListsComponent } from './your-lists.page';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    MatButtonModule,
     MatDatepickerModule,
     MatFormFieldModule,
     MatInputModule,
     MatNativeDateModule,
-    MatSelectModule,
+    MatSelectModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
