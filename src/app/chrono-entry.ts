e586cce104848d62830c720ddaaa2120248c357f
apheliana <65423514+apheliana@users.noreply.github.com>
2020-07-07 01:45:29 +0300

    Closes #10

--- src/app/chrono-entry.ts
+++ src/app/chrono-entry.ts
@@ -1,3 +1,4 @@
+import { format } from 'date-fns';
 import { ChronoList } from './chrono-list';
 
 export class ChronoEntry {
@@ -5,6 +6,9 @@ export class ChronoEntry {
   listId = 0;
   entryText = '';
   entryDate = new Date();
+  get entryDateText(): string {
+    return format(this.entryDate, 'dd.MM.yyyy');
+  }
   // TODO Let's add URLs in the next version
   // titleURL: string; //check if its valid URL, decapitalize all add http and www if necessary
   createdOn = new Date();
