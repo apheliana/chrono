f70a73aca6e396df778408860e1dbf3791a76383
coni2k <coni2k@users.noreply.github.com>
2020-07-26 15:12:20 +0200

    Unit test fixes

--- src/app/services/list.service.spec.ts
+++ src/app/services/list.service.spec.ts
@@ -7,7 +7,7 @@ describe('ListService', () => {
 
   beforeEach(() => {
     service = new ListService();
-    const user = new ChronoUser(0, '', '', '');
+    const user = new ChronoUser(0, '', '');
     service.users.push(user);
   });
 
@@ -21,7 +21,6 @@ describe('ListService', () => {
       const user = service.users[0];
       expect(user.userLists.length).toEqual(1);
       expect(user.userLists[0]).toBe(list);
-      expect(list.id).toEqual(0);
       expect(list.userId).toEqual(0);
       expect(list.name).toEqual('name');
       expect(list.description).toEqual(null);
@@ -34,7 +33,6 @@ describe('ListService', () => {
     service.createEntry(list, 'entryTitle', entryDate).subscribe((entry) => {
       expect(list.listItems.length).toEqual(1);
       expect(list.listItems[0]).toBe(entry);
-      expect(entry.id).toEqual(0);
       expect(entry.listId).toEqual(0);
       expect(entry.entryTitle).toEqual('entryTitle');
       expect(entry.entryDate).toEqual(entryDate);
@@ -44,7 +42,7 @@ describe('ListService', () => {
   describe('getListById tests', () => {
     it('should return the right list', () => {
       service.createList(0, 'name', 'desc').subscribe((list) => {
-        expect(service.getListById(0, 0)).toEqual(list);
+        expect(service.getListById(0, list.id)).toEqual(list);
       });
     });
 
