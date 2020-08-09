d8a6deef95ce2af6fe44bb16202adc2d56f82eb5
coni2k <coni2k@users.noreply.github.com>
2020-08-09 22:08:35 +0200

    Entry dialog stories

--- src/app/entries/dialog/entry-dialog.component.stories.ts
+++ src/app/entries/dialog/entry-dialog.component.stories.ts
@@ -1,40 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { action } from '@storybook/addon-actions';
 import { moduleMetadata } from '@storybook/angular';
-import { tap } from 'rxjs/operators';
 import { EntryDialogData } from './entry-dialog-data';
 import { EntryDialogModel } from './entry-dialog-model';
 import { EntryDialogComponent } from './entry-dialog.component';
 import { EntryDialogModule } from './entry-dialog.component.module';
 
 @Component({
-  template: '<button (click)="open()">open</button>',
+  template: '',
 })
-export class DialogTesterComponent {
-  constructor(private dialog: MatDialog) {
-    console.log('tester');
+class DialogTesterComponent implements OnInit {
+  @Input()
+  data: EntryDialogData = null;
+
+  @Output()
+  afterClosed = new EventEmitter<EntryDialogModel>();
+
+  constructor(private dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.open();
   }
 
-  open(): void {
-    const dialogRef = this.dialog.open<EntryDialogComponent, EntryDialogData>(EntryDialogComponent, {
-      data: {
-        model: {
-          entryTitle: '',
-          entryDate: new Date(),
-        },
-        viewMode: 'create',
-      },
+  private open(): void {
+    const dialogRef = this.dialog.open<EntryDialogComponent, EntryDialogData, EntryDialogModel>(EntryDialogComponent, {
+      data: this.data,
     });
 
-    dialogRef
-      .afterClosed()
-      .pipe(
-        tap((model: EntryDialogModel) => {
-          console.log('model', model);
-        })
-      )
-      .subscribe();
+    dialogRef.afterClosed().subscribe((model) => {
+      this.afterClosed.emit(model);
+    });
   }
 }
 
@@ -49,6 +46,30 @@ export default {
   ],
 };
 
-export const Default = () => ({
+export const Create = () => ({
   component: DialogTesterComponent,
+  props: {
+    data: {
+      model: {
+        entryTitle: '',
+        entryDate: new Date(),
+      },
+      viewMode: 'create',
+    },
+    afterClosed: action('afterClosed'),
+  },
+});
+
+export const Update = () => ({
+  component: DialogTesterComponent,
+  props: {
+    data: {
+      model: {
+        entryTitle: 'title?',
+        entryDate: new Date(2000, 1, 1),
+      },
+      viewMode: 'update',
+    },
+    afterClosed: action('afterClosed'),
+  },
 });
