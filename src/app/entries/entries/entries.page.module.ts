a1fa8867c90bfd381fc8e6324f702f7470335a3a
coni2k <coni2k@users.noreply.github.com>
2020-10-25 14:51:17 +0100

    Change "update" button

--- src/app/entries/entries/entries.page.module.ts
+++ src/app/entries/entries/entries.page.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { ManageListDialogModule } from 'src/app/lists/manage-list/manage-list.dialog.module';
 import { ManageEntryDialogModule } from '../manage-entry/manage-entry.dialog.module';
 import { EntriesPage } from './entries.page';
@@ -8,6 +9,6 @@ import { EntriesPage } from './entries.page';
 @NgModule({
   declarations: [EntriesPage],
   exports: [EntriesPage],
-  imports: [CommonModule, MatButtonModule, ManageEntryDialogModule, ManageListDialogModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, ManageEntryDialogModule, ManageListDialogModule],
 })
 export class EntriesPageModule {}
